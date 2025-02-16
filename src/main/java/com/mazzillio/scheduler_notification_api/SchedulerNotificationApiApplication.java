de7aa47cbccf5b6c7a2b22848d722c874cfe3ec2
mazzillio <mateca500@gmail.com>
2025-02-16 10:04:36 -0300

    setup

---
+++ src/main/java/com/mazzillio/scheduler_notification_api/SchedulerNotificationApiApplication.java
@@ -0,0 +1,13 @@
+package com.mazzillio.scheduler_notification_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerNotificationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerNotificationApiApplication.class, args);
+	}
+
+}
